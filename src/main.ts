f64e4d03fb7c691b9b93cdf8c950c8a6e4b8ee19
Pierre-Olivier Blouin <pierre-olivier.blouin@alayacare.com>
2020-05-26 17:55:31 -0400

    WIP: Working api call

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import Vue from 'vue';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 import App from './App.vue';
 import router from './router';
+ 
+axios.defaults.baseURL = process.env.VUE_APP_SERVER_URL;
 
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false;
 
 new Vue({
